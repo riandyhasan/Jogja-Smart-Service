5890ba60fb11f0b9062e71e612598b66f6ee6223
riandyhasan <riandyhsn@gmail.com>
2022-12-03 18:15:20 +0700

    change database instance

--- be/database.py
+++ be/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import pymysql
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:@34.87.2.10/jss"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:@34.172.226.77/jss"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
