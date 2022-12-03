29e10dc486d201cd877796e5dc45af46d961d34b
riandyhasan <riandyhsn@gmail.com>
2022-12-03 17:33:46 +0700

    add cloud sql

--- be/database.py
+++ be/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import pymysql
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./jss.db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:@34.87.2.10/jss"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
